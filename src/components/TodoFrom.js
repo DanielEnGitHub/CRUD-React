770a7ccb5f5b60352164898e838a2d060c7d85c7
Daniel <danieljgallina@gmail.com>
2022-02-08 22:41:59 -0600

    Mostrando estados y errores

--- src/components/TodoFrom.js
+++ src/components/TodoFrom.js
@@ -9,6 +9,8 @@ const TodoForm = ( {todoAdd} ) =>{
 
     const [formValues, setFormValues] = useState(initialForm);
     const {tittle, description} = formValues;
+    const [error, setError] = useState(null);
+    const [successMS, setsuccessMS] = useState(null);
 
     const handleInputChange = e => {
         const changedFormValues = {
@@ -21,15 +23,33 @@ const TodoForm = ( {todoAdd} ) =>{
     
     const handleSubmit = (e) =>{
         e.preventDefault();
+        if (tittle.trim() === ''){
+            setError('No puedes dejar el titulo en blanco');
+            return;
+        }
 
+        if (description.trim() === ''){
+            setError('No puedes dejar la descripcion en blanco');
+            return;
+        }
         todoAdd(formValues);
+
+
+        setsuccessMS('La nueva tarea se agrego correctamente');
+        setTimeout(() =>{
+            setsuccessMS(null);
+        }, 2000)
+
+        setFormValues(initialForm);
+        setError(null);
+        
     }
 
     return (
         <div>
             <h1>Nueva tarea</h1>
             <form onSubmit={handleSubmit}>
-                <input 
+                <input
                     type='text'
                     placeholder='Titulo'
                     className='form-control'
@@ -52,6 +72,25 @@ const TodoForm = ( {todoAdd} ) =>{
                     Agregar Tarea
                 </button>
             </form>
+
+            {
+                error 
+                && (
+                    <div className='alert alert-danger mt-2'>
+                        {error}
+                    </div>
+                )
+            }
+
+            {
+                successMS
+                && (
+                    <div className='alert alert-success mt-2'>
+                        {successMS}
+                    </div>
+                )
+            }
+            
         </div>
     );
 };
